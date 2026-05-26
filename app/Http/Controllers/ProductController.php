aa4b64b289233fd644c43e55373c3a0d405447e8
Cyndi Burns <cburnscode@gmail.com>
2026-05-26 11:55:22 +1000

    feat: Include categories in product and search views for enhanced context

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -10,20 +11,26 @@ class ProductController extends Controller
 {
     public function show($id)
     {
+        $categories = Category::all();
         $product = Product::findOrFail($id);
-        return view('products.product', compact('product'));
+        return view('products.product', compact('product', 'categories'));
     }
 
     public function search(Request $request)
     {
         $searchTerm = $request->input('query');
-
-        $singularTerm = Str::singular($searchTerm);
-
+        $singularTerm = \Illuminate\Support\Str::singular($searchTerm);
+        $categories = \App\Models\Category::all();
         $products = Product::where('name', 'LIKE', '%' . $searchTerm . '%')
             ->orWhere('name', 'LIKE', '%' . $singularTerm . '%')
             ->get();
 
-        return view('products.search', compact('products', 'searchTerm'));
+
+        return view('products.search', [
+            'products' => $products,
+            'categories' => $categories,
+            'query' => $searchTerm,
+            'searchTerm' => $searchTerm
+        ]);
     }
 }
