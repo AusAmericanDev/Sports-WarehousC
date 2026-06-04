99199a59d0a1c6f8cc40f3a732859eca619ab2c7
Cyndi Burns <cburnscode@gmail.com>
2026-06-04 20:46:36 +1000

    fix: added static public css asset track to bypass server build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
-    @vite(['resources/css/style.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
--- resources/views/product/search.blade.php
+++ resources/views/product/search.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
 <section class="products-section">
-
     <div class="container">
 
         <h1 class="page-title">
@@ -11,77 +9,44 @@
         </h1>
 
         <div class="products-grid">
-
             @forelse($products as $product)
-
             <article class="product-card">
-
-                <a href="{{ route('products.show', $product->id) }}" class="product-card-link">
-
+                <a href="{{ url('/products/' . $product->id) }}" class="product-card-link">
                     <h3 class="visually-hidden">
                         {{ $product->name }}
                     </h3>
 
                     <div class="product-img-wrapper">
-
-                        <img
-                            src="{{ asset('images/' . $product->image_path) }}"
-                            alt="{{ $product->name }}">
-
+                        <img src="{{ asset('images/' . $product->image_path) }}" alt="{{ $product->name }}">
                     </div>
 
                     <div class="product-info-wrapper">
-
                         <div class="price-container">
-
                             @if($product->sale_price)
-
                             <span class="current-price">
                                 ${{ number_format($product->sale_price, 2) }}
                             </span>
-
                             <span class="was-price">
-
-                                <span class="was-label">
-                                    WAS
-                                </span>
-
-                                <del class="original-price">
-                                    ${{ number_format($product->price, 2) }}
-                                </del>
-
+                                <span class="was-label">WAS</span>
+                                <del class="original-price">${{ number_format($product->price, 2) }}</del>
                             </span>
-
                             @else
-
                             <span class="price">
                                 ${{ number_format($product->price, 2) }}
                             </span>
-
                             @endif
-
                         </div>
-
                         <p class="product-name">
                             {{ $product->name }}
                         </p>
-
                     </div>
-
                 </a>
-
             </article>
-
             @empty
-
-            <p>No products found matching "{{ $searchTerm }}".</p>
-
+            <p class="no-results-text">No products found matching "{{ $searchTerm }}".</p>
             @endforelse
-
         </div>
 
     </div>
-
 </section>
-
 @endsection
