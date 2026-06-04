cc9a3136a48330680f894bd2fc00e3eb63fbec38
Cyndi Burns <cburnscode@gmail.com>
2026-06-04 21:20:31 +1000

    fix: migrations

--- database/migrations/2026_06_04_105758_create_orders_table.php
+++ database/migrations/2026_06_04_105758_create_orders_table.php
@@ -1,17 +1,37 @@
-public function up(): void
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-Schema::create('orders', function (Blueprint $table) {
-$table->id(); // This acts as the Order Number
-$table->string('first_name');
-$table->string('last_name');
-$table->string('delivery_address');
-$table->string('contact_number');
-$table->string('email_address');
-// Assessment requirements (storing payment data directly)
-$table->string('card_number');
-$table->string('expiry_date');
-$table->string('card_name');
-$table->decimal('total_price', 10, 2);
-$table->timestamps();
-});
-}
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id(); // This acts as the Order Number
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('delivery_address');
+            $table->string('contact_number');
+            $table->string('email_address');
+            // Assessment requirements (storing payment data directly)
+            $table->string('card_number');
+            $table->string('expiry_date');
+            $table->string('card_name');
+            $table->decimal('total_price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
